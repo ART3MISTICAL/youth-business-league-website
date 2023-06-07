3745d4a7378ea1b9408592373141b1e5fd3b9ea8
Devansh Arora <dev.marvel.avengers@gmail.com>
2023-06-07 09:54:31 +0530

    commit

--- components/main.tsx
+++ components/main.tsx
@@ -22,9 +22,9 @@ const Main = () => {
 		<section className="absolute z-10 opacity-100 md:w-[500px] h-[250px] w-[250px] md:h-[500px] bg-[#021915] rounded-full notonscreen up">
 			<Image src="/logo.png" className='absolute z-10 opacity-100 transition-all ease-in duration-[2s]' width={500} height={500} alt="logo" />
 		</section>
-			<a href='#who' className='bg-[#021915] rounded-full absolute z-10 w-16 h-16 bottom-5 flex justify-center items-center cursor-pointer notonscreen up border-[1.5px] border-[#f7aa1b]' data-scroll-to data-scroll-target="#who">
+			<a href='#who' className='bg-[#021915] rounded-full absolute z-10 w-16 h-16 bottom-5 flex justify-center items-center cursor-pointer notonscreen up border-[1.5px] border-[#f7aa1b] hover:border-[#021915] hover:bg-[#f7aa1b] text-[#f7aa1b] hover:text-[#021915]'>
 			{/*<IconButton color="blue" size="lg" variant="gradient" className="rounded-full absolute z-10 w-20 h-20 bottom-5">*/}
-				<i className="fa-sharp fa-solid fa-arrow-down text-[#f7aa1b]"></i>
+				<i className="fa-sharp fa-solid fa-arrow-down "></i>
       		{/*</IconButton>*/}
 			</a>
 		</div>
